132314e40b2b5d8d2a2808430efda92b8e42e22e
Sidney San Martín <s@sidneysm.com>
2011-06-20 20:05:17 -0400

    Support for required arguments before the options argument.

--- argment.js
+++ argment.js
@@ -1,9 +1,21 @@
-function argment(callback, params){
-	return function(inArgs){
-		var args = [], i, l;
-		for (i=0, l = params.length; i < l; i++) {
-			args.push(inArgs[params[i]]);
+function argment(params, required, callback){
+	if (typeof required === 'function') {
+		callback = required;
+		required = 0;
+	}
+	return function(){
+		var args = [], options, i = 0, l;
+		if (required) {
+			for (i = 0; i < required; i++) {
+				args.push(arguments[i]);
+			}
+		}
+		options = arguments[i];
+		if (options) {
+			for (i = 0, l = params.length; i < l; i++) {
+				args.push(options[params[i]]);
+			}
 		}
 		return callback.apply(this, args);
-	}
+	};
 }
